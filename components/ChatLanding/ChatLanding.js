724a7b7576e848a490aeb3b3753fe150e163bd5a
giorgio <giorgiosavron@me.com>
2023-06-01 12:47:41 -0400

    mobile layout

--- components/ChatLanding/ChatLanding.js
+++ components/ChatLanding/ChatLanding.js
@@ -15,16 +15,16 @@ export const ChatLanding = () => {
       />
       <h1>Chatty AI</h1>
       <div className="flex flex-col items-center gap-5">
-        <div className="rounded-lg bg-[#3C4655] px-20 shadow-md">
-          <div className="flex items-center gap-6">
+        <div className="rounded-lg bg-[#3C4655] px-16 shadow-md md:px-20">
+          <div className="flex items-center gap-2 md:gap-6">
             <FontAwesomeIcon
               icon={faTerminal}
               fade
               className="h-6 w-6 text-xl text-[#BFBE44]"
             />
-            <h5> What can you ask me?</h5>
+            <h5 className="text-base md:text-lg"> What can you ask me?</h5>
           </div>
-          <div>
+          <div className="text-sm md:text-base">
             <ul>
               <li>What is your name?</li>
               <li>Who made you?</li>
@@ -32,24 +32,24 @@ export const ChatLanding = () => {
             </ul>
           </div>
         </div>
-        <div className="rounded-lg bg-[#3C4655] px-16 shadow-md">
-          <div className="flex items-center gap-6">
+        <div className="rounded-lg bg-[#3C4655] px-12 shadow-md md:px-16">
+          <div className="flex items-center gap-2 md:gap-6">
             <FontAwesomeIcon
               icon={faMicrochip}
               className="h-6 w-6 text-xl text-[#BFBE44]"
             />
-            <h5> What am I capable of?</h5>
+            <h5 className="text-base md:text-lg"> What am I capable of?</h5>
           </div>
-          <div>
+          <div className="text-sm md:text-base">
             <ul>
-              <li>I can save last 5 conversations</li>
+              <li>I can save our conversations</li>
               <li>I can respond to follow up corrections</li>
               <li>I am trained on most data up to 2021</li>
             </ul>
           </div>
         </div>
 
-        <div className=" text-xs">
+        <div className="text-xs">
           Chatty-AI may produce inaccurate information about people, places, or
           facts.
         </div>
